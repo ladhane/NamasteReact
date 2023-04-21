3effd7215849dfbfec025208c4b139d3c70f7d22
ladhane <40428731+ladhane@users.noreply.github.com>
2023-04-21 10:16:00 +0530

    Add files via upload

--- Day10/src/app.js
+++ Day10/src/app.js
@@ -1,1 +1,59 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Body } from "./components/Body";
+import Profile from "./components/ProfileClass";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import RestaurantMenu from "./components/RestaurantMenu";
+import BasicExample from "./components/Login";
 
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+        {
+            path: "/",
+            element: <Body />,
+          },
+        {
+            path: "/about",
+            element: <About />,
+            children:[{
+                path:'profile',
+                element: <About/>
+            }]
+          },
+          {
+            path: "/contact",
+            element: <Contact />,
+          },
+          {
+            path: "/restaurant/:id",
+            element: <RestaurantMenu/>,
+          },
+          {
+            path: "/login",
+            element: <BasicExample/>,
+          }
+    ]
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
