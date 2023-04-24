f17e371e9884210fafb9eaab95a18ad223325849
ladhane <40428731+ladhane@users.noreply.github.com>
2023-04-24 10:22:29 +0530

    Add files via upload

---
+++ Day11/src/components/Footer.js
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import UserContext from "../utils/UserContext";
+
+export const Footer = ()=> {
+    const {user} = useContext(UserContext);
+    return(
+        <div className="fixed bottom-0 left-0 z-20 w-full bg-slate-100 shadow dark:bg-gray-900">
+        <p className="text-lg text-center mb-0 p-5"> This site is developd by {user.name}. You can contact the team via email {user.email}</p>
+        </div>
+    )
+}
